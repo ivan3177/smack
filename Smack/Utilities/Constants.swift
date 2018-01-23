62d078db8f2491660f7c81daaa1076910b4f9939
Ivan Sokolovskii <ivan3177@gmail.com>
2018-01-23 23:53:57 +0300

    Moved API server to remote host and changed it's URL

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 typealias CompletionHandler = (_ Success: Bool) -> ()
 
 // URL Constants
-let BASE_URL = "http://localhost:3005/v1"
+let BASE_URL = "http://isokol-dev.ru:3005/v1"
 let URL_REGISTER = "\(BASE_URL)/account/register"
 let URL_LOGIN = "\(BASE_URL)/account/login"
 let URL_CREATE_USER = "\(BASE_URL)/user/add"
