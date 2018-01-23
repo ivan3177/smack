a9a6acb8e588372ffc87d81d45aa4305dfb2623c
Ivan Sokolovskii <ivan3177@gmail.com>
2018-01-23 14:14:30 +0300

    Add login screen Added LoginVC class for new view controller Added new view controller to storyboard and set up all elements in it Created segue from ChannelsVC to open login screen Added action to dismiss login screen

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Иван Соколовский on 23.01.2018.
+//  Copyright © 2018 iSOKOL DEV. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closeTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Иван Соколовский on 23.01.2018.
+//  Copyright © 2018 iSOKOL DEV. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
